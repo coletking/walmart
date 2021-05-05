7ef55c2db656cb17a38ae1ea0601db73b6910b8c
julius kingsley <coletkingconcept@gmail.com>
2021-05-05 09:26:31 +0100

    added new page

--- js/index.js
+++ js/index.js
@@ -21,18 +21,18 @@ $("#theTarget").skippr({
 
 $(document).ready(function(){
 
-    $("#theTarget1").skippr();
+    $(".theTarget").skippr();
 
 });
 
-$("#theTarget1").skippr({
+$(".theTarget").skippr({
 
     transition: 'fade',
     speed: 1000,
     easing: 'easeOutQuart',
     navType: 'bubble',
     childrenElementType: 'div',
-    arrows: false,
+    arrows: true,
     autoPlay: true,
     autoPlayDuration: 2000,
     keyboardOnAlways: true,
